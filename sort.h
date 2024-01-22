6745ac24fa9c10545e954a4371d2c76b6a1bb0d3
Idowu Olayiwola Bamidele <idowu.olayiwola.bamidele@gmail.com>
2024-01-22 01:12:08 +0100

    Shell sort - Knuth Sequence

--- sort.h
+++ sort.h
@@ -26,6 +26,7 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+void shell_sort(int *array, size_t size);
 
 /* Function prototype for print_list and print_array function */
 void print_list(const listint_t *list);
